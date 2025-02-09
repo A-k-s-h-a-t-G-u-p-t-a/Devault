49f1c50a6478926bc88c0d96990769164f7492e9
Akshat <akshatgupta1673@gmail.com>
2025-02-09 16:17:25 +0530

    Initial commit with new Next.js project

---
+++ src/app/home/page.tsx
@@ -0,0 +1,15 @@
+import ProjectCard from '../components/projectcard';
+import Navbar from '../components/navbar';
+export default function Home() {
+    return <div>
+        <Navbar />
+        <ProjectCard
+         title="DeFi Protocol"
+         description="Decentralized finance protocol for crypto trading"
+         tags={["React", "Solidity", "Node.js"]}
+         funding="5.0"
+         issues={12}
+         progress={75}
+        />
+    </div>
+}
