83bb292fd9d0edd0e27e7f173787465c7e0e896e
Akshat <akshatgupta1673@gmail.com>
2025-03-06 23:15:10 +0530

    token added

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -8,10 +8,17 @@ import {
   export const client = createThirdwebClient({
     clientId: "93c34b41ecff9e5cdcdb5b22d593a02b",
   });
-  
+  export const client2 = createThirdwebClient({
+    clientId: "9da4c23ac769c027ad5d9ae9b0952a15"
+  }); 
   // connect to your contract
   export const contract = getContract({
     client,
     chain: defineChain(11155111),
     address: "0xA39DF769398c5ca177A84F8719e645A8B23f8F09",
+  });
+  export const contract2 = getContract({
+    client,
+    chain: defineChain(11155111),
+    address: "0xBb0F165109dAA2007FbeeE6b4a4785984C919E56",
   });
